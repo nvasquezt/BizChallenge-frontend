285d1fe013699a311fafb865d03a9d12cfccd016
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-08-03 08:06:11 -0500

    second change to deployment

--- src/Components/MainRouter/index.jsx
+++ src/Components/MainRouter/index.jsx
@@ -3,12 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
   FAVORITES_ROUTE,
   HOME_ROUTE,
-  LOGIN_ROUTE,
   PROFILE_ROUTE,
   MOVIE_ROUTE,
 } from '../../Constants';
 import Home from '../../Pages/Home';
-import Login from '../../Pages/Login';
 import Profile from '../../Pages/Profile';
 import Favorites from '../../Pages/Favorites';
 import NavBar from '../NavBar';
@@ -20,7 +18,6 @@ const MainRouter = () => {
       <NavBar />
       <Routes>
         <Route path={HOME_ROUTE} element={<Home />} />
-        <Route path={LOGIN_ROUTE} element={<Login />} />
         <Route path={PROFILE_ROUTE} element={<Profile />} />
         <Route path={FAVORITES_ROUTE} element={<Favorites />} />
         <Route path={MOVIE_ROUTE} element={<MovieDetail />} />
--- src/Constants/index.jsx
+++ src/Constants/index.jsx
@@ -1,5 +1,4 @@
 export const HOME_ROUTE = '/';
-export const LOGIN_ROUTE = '/login';
 export const PROFILE_ROUTE = '/profile';
 export const FAVORITES_ROUTE = '/favorites';
 export const MOVIE_ROUTE = '/movie/:id';
